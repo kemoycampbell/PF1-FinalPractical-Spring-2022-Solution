3e75e127f2dec17dd092a578c777b067588a5d8f
kemoycampbell <ksc2650@rit.edu>
2022-04-22 15:58:34 -0400

    solution to final practical exam

---
+++ WorkingWithJSON/GameSale.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithJSON
+{
+    class GameSale
+    {
+
+        private const string DEFAULT_NAME = "Name not provided";
+        private const string DEFAULT_PLATFORM = "Platform not provided";
+        private const string DEFAULT_DATE = "Date not provided";
+
+        private string name;
+        private string date;
+        private string platform;
+        private int totalCopies;
+        private double totalRevenues;
+
+        public GameSale(string name, string platform, string date,int totalCopies, double totalRevenues)
+        {
+            Name = name;
+            Date = date;
+            Platform = platform;
+            TotalCopies = totalCopies;
+            TotalRevenues = totalRevenues;
+        }
+
+        public string Name { get => name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    name = DEFAULT_NAME;
+                else
+                    name = value;
+            }
+        }
+        public string Date { get => date;
+            set
+            {
+                if (DateTime.TryParse(value, out DateTime d))
+                    date = value;
+                else
+                    date = DEFAULT_DATE;
+            }
+        }
+        public string Platform { get => platform;
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    platform = value;
+                else
+                    platform = DEFAULT_PLATFORM;
+            }
+        }
+        public int TotalCopies { get => totalCopies; 
+            set
+            {
+                if (value >= 0)
+                    totalCopies = value;
+            }
+                
+        }
+        public double TotalRevenues { get => totalRevenues;
+            set
+            {
+                if (value >= 0)
+                    totalRevenues = value;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Name} {Platform} {Date} {TotalCopies} {TotalRevenues}";
+        }
+    }
+}
---
+++ WorkingWithJSON/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+namespace WorkingWithJSON
+{
+    class Program
+    {
+        static List<GameSale> records = new List<GameSale>();
+        static void Main(string[] args)
+        {
+            //REMOVE THIS BEFORE SUBMIT
+            //records.Add(new GameSale("Test", "Testing Platform1", "10/10/2020", 100, 2500));
+            //records.Add(new GameSale("Test1", "Testing Platform2", "10/10/2020", 100, 2500));
+            //records.Add(new GameSale("Test2", "Testing Platform3", "10/10/2020", 100, 2500));
+            //records.Add(new GameSale("Test3", "Testing Platform4", "10/10/2020", 100, 2500));
+            //records.Add(new GameSale("Test3", "Testing Platform5", "10/10/2020", 100, 2500));
+
+            while (true)
+            {
+                Console.WriteLine("1. Create new Sales\n2. Remove a record\n3. Show all sale records\n" +
+                    "4. Write records to file\n5. Read in sales from file\n" +
+                    "6. Exit");
+
+                Console.Write("Selection:");
+                switch(Console.ReadLine())
+                {
+                    case "1":
+                        Console.Write("Name:");
+                        string name = Console.ReadLine();
+                        Console.Write("Platform:");
+                        string platform = Console.ReadLine();
+                        Console.Write("Date:");
+                        string date = Console.ReadLine();
+                        Console.Write("Total Copies Sold:");
+                        int copies = int.Parse(Console.ReadLine());
+                        Console.Write("Total Revenues:");
+                        double revenues = double.Parse(Console.ReadLine());
+
+                        GameSale sale = new GameSale(name, platform, date, copies, revenues);
+                        records.Add(sale);
+                        break;
+                    case "2":
+                        Console.Write("Name:");
+                        name = Console.ReadLine().ToLower();
+                        Console.Write("Platform:");
+                        platform = Console.ReadLine().ToLower();
+
+                        bool deleted = false;
+                        foreach(GameSale _sale in records)
+                        {
+                            if(_sale.Name.ToLower() == name && _sale.Platform.ToLower() == platform)
+                            {
+                                records.Remove(_sale);
+                                deleted = true;
+                                break;
+                            }
+                        }
+
+                        if (!deleted)
+                        {
+                            Console.WriteLine($"No record found for {name} - {platform}");
+                        }
+
+                        break;
+
+                    case "3":
+                        foreach(GameSale _sale in records)
+                            Console.WriteLine(_sale);
+                        break;
+                    case "4":
+                        Console.Write("Filename:");
+                        string filename = Console.ReadLine();
+
+
+
+                        //using(StreamWriter writer = new StreamWriter(filename))
+                        //{
+                        //    foreach (GameSale _sale in records)
+                        //    {
+                        //        writer.WriteLine($"{_sale.Name},{_sale.Platform},{_sale.Date},{_sale.TotalCopies},{_sale.TotalRevenues}");
+                        //    }
+                        //}
+
+
+                        StreamWriter writer = new StreamWriter(filename);
+                        string _info = JsonConvert.SerializeObject(records);
+                        writer.WriteLine(_info);
+
+                        //foreach (GameSale _sale in records)
+                        //{
+                        //    writer.WriteLine($"{_sale.Name},{_sale.Platform},{_sale.Date},{_sale.TotalCopies},{_sale.TotalRevenues}");
+                        //}
+
+                        writer.Close();
+                        Console.WriteLine("File created!");
+                            
+                        
+                        break;
+                    case "5":
+
+                        Console.Write("Filename:");
+                        filename = Console.ReadLine();
+
+                        if (File.Exists(filename))
+                        {
+                            StreamReader reader = new StreamReader(filename);
+                            _info = reader.ReadToEnd();
+
+                            records = JsonConvert.DeserializeObject<List<GameSale>>(_info);
+
+
+                            //while(!reader.EndOfStream)
+                            //{
+                            //    string[] info = reader.ReadLine().Split(",");
+                            //    sale = new GameSale(info[0], info[1], info[2], int.Parse(info[3]), double.Parse(info[4]));
+                            //    records.Add(sale);
+                            //}
+
+                            reader.Close();
+                            Console.WriteLine("All records has been loaded");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{filename} not found");
+                        }
+                        break;
+                    case "6":
+                        Console.WriteLine("Thank you for using this system. Goodbye... :-(");
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection!");
+                        break;
+
+                }
+            }
+        }
+    }
+}
